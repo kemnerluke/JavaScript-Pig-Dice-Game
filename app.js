0dbca769c3f6ede8bf2d9cca196b0f14d0b3a87b
Luke <kemnerluke@gmail.com>
2020-04-09 15:20:10 -0400

    implemented updated score and changing active player functionality

--- app.js
+++ app.js
@@ -39,6 +39,27 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3 update round score only if rolled number was not a 1
+    if (dice !== 1){
+        // add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('dice').style.display = 'none';
+
+
+    }
+
 
 });
 
